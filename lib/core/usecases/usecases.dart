8cfc8de59889583c5761d3f8b3975ebb77cfe256
josephdsbr <josephdsbr@gmail.com>
2020-07-27 22:15:56 -0300

    implementando testes e funcionalidades par abuscar um número trivial de forma randomizada

---
+++ lib/core/usecases/usecases.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:tddCleanArchitectureApp/core/error/failure.dart';
+
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);
+}
+
+class NoParams extends Equatable {}
--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -1,23 +1,15 @@
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
 import 'package:tddCleanArchitectureApp/core/error/failure.dart';
 import 'package:tddCleanArchitectureApp/core/usecases/usecases.dart';
 import 'package:tddCleanArchitectureApp/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:tddCleanArchitectureApp/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
-class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
+class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
   final NumberTriviaRepository repository;
-  GetConcreteNumberTrivia(this.repository);
+  GetRandomNumberTrivia(this.repository);
 
   @override
-  Future<Either<Failure, NumberTrivia>> call(Params params) async {
-    return await repository.getConcreteNumberTrivia(params.number);
+  Future<Either<Failure, NumberTrivia>> call(NoParams params) async {
+    return await repository.getRandomNumberTrivia();
   }
-}
-
-class Params extends Equatable {
-  final int number;
-
-  Params({@required this.number}) : super([number]);
 }
